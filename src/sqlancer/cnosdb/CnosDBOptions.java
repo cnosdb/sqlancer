df9baec0fe14945ec12670b26b5878a567f6f73e
ZuoTiJia <2239651886@qq.com>
2023-01-18 16:20:44 +0800

    remove unused

--- src/sqlancer/cnosdb/CnosDBOptions.java
+++ src/sqlancer/cnosdb/CnosDBOptions.java
@@ -6,6 +6,7 @@ import sqlancer.DBMSSpecificOptions;
 import sqlancer.OracleFactory;
 import sqlancer.cnosdb.CnosDBOptions.CnosDBOracleFactory;
 import sqlancer.cnosdb.oracle.CnosDBNoRECOracle;
+import sqlancer.cnosdb.oracle.CnosDBPivotedQuerySynthesisOracle;
 import sqlancer.cnosdb.oracle.tlp.CnosDBTLPAggregateOracle;
 import sqlancer.cnosdb.oracle.tlp.CnosDBTLPHavingOracle;
 import sqlancer.cnosdb.oracle.tlp.CnosDBTLPWhereOracle;
